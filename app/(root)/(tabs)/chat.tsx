be1dfa860b524ef19ca2d270b370a3213513fa06
Radu1409 <radu.purecel20@gmail.com>
2024-11-03 13:06:55 +0200

    app: root: Add sign out button. Fix text and bg color

--- app/(root)/(tabs)/chat.tsx
+++ app/(root)/(tabs)/chat.tsx
@@ -1,13 +1,31 @@
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-import { images } from "@/constants";
+import { icons, images } from "@/constants";
+import { router } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 const Chat = () => {
+  const { signOut } = useAuth();
+
+  const handleSignOut = () => {
+    signOut().then(() => {});
+
+    router.replace("/(auth)/sign-in");
+  };
+
   return (
-    <SafeAreaView className="flex-1 bg-white p-5">
+    <SafeAreaView className="flex-1 bg-general-500 p-5">
       <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <Text className="text-2xl font-JakartaBold">Chat</Text>
+        <View className="flex flex-row items-center justify-between my-5">
+          <Text className="text-2xl font-JakartaBold">Chat List</Text>
+          <TouchableOpacity
+            onPress={handleSignOut}
+            className="justify-center items-center w-10 h-10 rounded-full bg-white"
+          >
+            <Image source={icons.out} className="w-4 h-4" />
+          </TouchableOpacity>
+        </View>
         <View className="flex-1 h-fit flex justify-center items-center">
           <Image
             source={images.message}
